a1ad4d04459c3b7c7941506f3c86db0cff483b45
Christopher Taylor <c.a.taylor1990@gmail.com>
2024-02-28 15:35:32 -0500

    Clean up code

--- src/core.ts
+++ src/core.ts
@@ -14,11 +14,6 @@ import {
   visit,
   visitWithTypeInfo,
   isInputType,
-  GraphQLEnumType,
-  GraphQLInputObjectType,
-  GraphQLInputType,
-  getNullableType,
-  isEnumType,
 } from 'graphql';
 import {
   DocumentNode,
@@ -30,6 +25,8 @@ import {
   isNode,
 } from 'graphql/language/ast';
 import { memoize } from 'lodash';
+import { mapIfArray } from './mapIfArray';
+import Serializer from './serializer';
 
 interface BaseNodeInQuery {
   /**
@@ -136,63 +133,6 @@ function unpackTypeInner(type: GraphQLOutputType): GraphQLOutputType | void {
 
 function unpackType(type: GraphQLOutputType): GraphQLScalarType | void {
   return unpackTypeInner(type) as GraphQLScalarType | void;
-}
-
-export class Serializer {
-  constructor(
-    readonly schema: GraphQLSchema,
-    readonly scalars: Record<string, GraphQLScalarType<any, any>>
-  ) {}
-
-  public serialize(value: any, type: GraphQLInputType): any {
-    if (isNonNullType(type)) {
-      return this.serializeInternal(value, getNullableType(type));
-    } else {
-      return this.serializeNullable(value, getNullableType(type));
-    }
-  }
-
-  protected serializeNullable(value: any, type: GraphQLInputType): any {
-    return this.serializeInternal(value, type);
-  }
-
-  protected serializeInternal(value: any, type: GraphQLInputType): any {
-    if (value === null || value === undefined) {
-      return value;
-    }
-
-    if (isScalarType(type) || isEnumType(type)) {
-      return this.serializeLeaf(value, type);
-    }
-
-    if (isListType(type)) {
-      return mapIfArray(value, (v) => this.serialize(v, type.ofType));
-    }
-
-    return this.serializeInputObject(value, type);
-  }
-
-  protected serializeLeaf(
-    value: any,
-    type: GraphQLScalarType | GraphQLEnumType
-  ): any {
-    const fns = this.scalars[type.name] || type;
-    return fns.serialize(value);
-  }
-
-  protected serializeInputObject(
-    givenValue: any,
-    type: GraphQLInputObjectType
-  ): any {
-    let value = givenValue;
-    const ret: any = {};
-    const fields = type.getFields();
-    for (const [key, val] of Object.entries(value)) {
-      const f = fields[key];
-      ret[key] = f ? this.serialize(val, f.type) : val;
-    }
-    return ret;
-  }
 }
 
 export class CustomScalarResolver {
@@ -413,11 +353,4 @@ export class CustomScalarResolver {
     }
     return operation;
   }
-}
-
-function mapIfArray<TOther, TItem, TResponse>(
-  a: TOther | TItem[],
-  fn: (x: TItem) => TResponse
-): TOther | TResponse[] {
-  return Array.isArray(a) ? a.map(fn) : a;
 }
